763e2b262c738d4acfc3d3e0945b6a6e9b2bd86e
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-10-15 11:26:19 +0100

    EDIT: edited test files

---
+++ src/test/java/AnimalPackage/LionessTest.java
@@ -0,0 +1,20 @@
+package AnimalPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LionessTest {
+
+    @Test
+    void isPregnant() {
+    }
+
+    @Test
+    void setPregnant() {
+    }
+
+    @Test
+    void fly() {
+    }
+}
---
+++ src/test/java/SuperheroPackage/SpidermanTest.java
@@ -0,0 +1,28 @@
+package SuperheroPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpidermanTest {
+
+    @Test
+    void fly() {
+    }
+
+    @Test
+    void isSuitedUp() {
+    }
+
+    @Test
+    void setSuitedUp() {
+    }
+
+    @Test
+    void getSuit() {
+    }
+
+    @Test
+    void setSuit() {
+    }
+}
